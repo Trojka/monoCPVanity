7de097a57114df2c2a9b8d0ff5a674b28ab5d122
sergedesmedt <sergedesmedt@yahoo.com>
2014-10-21 22:07:22 +0200

    added FileStorageService

---
+++ monoCPVanity/Util/FileStorageService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace touchCPVanity.Util
+{
+	public class FileStorageService
+	{
+		public string BaseFolder {
+			get {
+				return Environment.GetFolderPath (Environment.SpecialFolder.MyDocuments);
+			}
+		}
+
+		public void WriteBytes(byte[] data, string filename)
+		{
+			var filePath = Path.Combine (BaseFolder, filename);
+			File.WriteAllBytes (filePath, data);
+		}
+
+		public byte[] ReadBytes(string filename)
+		{
+			var filePath = Path.Combine (BaseFolder, filename);
+			return File.ReadAllBytes (filePath);
+		}
+
+		public void DeleteFile(string filename)
+		{
+			var filePath = Path.Combine (BaseFolder, filename);
+			File.Delete (filePath);
+		}
+
+		public bool FileExists(string filename)
+		{
+			var filePath = Path.Combine (BaseFolder, filename);
+			return File.Exists (filePath);
+		}
+	}
+}
+
